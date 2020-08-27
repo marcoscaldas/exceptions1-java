a5ad52f4fda2e6e1856f3f15205c959e7740077b
marcoscaldas <donarumo@hotmail.com>
2020-08-27 14:39:24 -0300

    Very bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,62 @@
+package application;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {// adiciona throws parseException devido o sdf.parse
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+		
+		System.out.print("Room numer: ");
+		int number = sc.nextInt();
+		System.out.println("Check-in date (dd/mm/yyy)");
+		Date checkIn = (Date) sdf.parse(sc.next()); //entra com um string e o sdf.parser converte
+		System.out.println("Check-out date (dd/mm/yyy)");
+		Date checkOut = (Date) sdf.parse(sc.next());
+		
+		if (!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Check-out date must be after check-in date");
+		}
+		else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);	
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation: ");
+			System.out.println("Check-in date (dd/mm/yyy)");
+			checkIn = (Date) sdf.parse(sc.next()); //entra com um string e o sdf.parser converte
+			System.out.println("Check-out date (dd/mm/yyy)");
+			checkOut = (Date) sdf.parse(sc.next());
+			
+			Date now = new Date();
+			
+			if (checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future dates ");
+			}
+			else if (!checkOut.after(checkIn))
+			{
+				System.out.println("Error in reservation: Check-out date must be after check-in date");	
+			}
+			else
+			{
+			reservation.updateDates(checkIn, checkOut);			
+			System.out.println("Reservation: " + reservation);
+			}
+		}
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
